ad41fc253517137b86948e89c9b8f18399500700
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2022-05-09 12:51:53 +0530

    Add files via upload

---
+++ components/data.js
@@ -0,0 +1,15 @@
+export const projectsList = [
+  {
+    id: 1,
+    name: "Secret Pittsburgh",
+    description:
+      "Website for the Secret Pittsburgh class at the University of Pittsburgh (ENGLIT 1412) dedicated to uncovering hidden spaces and unusual places in the city of Pittsburgh.",
+    image: "/images/secret_pittsburgh.png",
+    mobileImage: "/images/secret_pittsburgh_small.png",
+    link: "https://secretpittsburgh.org/",
+    GitHub: "https://github.com/GameDog9988/secret-pittsburgh",
+    project: "secret-pittsburgh",
+    tools: ["React/Gatsby", "Tailwind CSS", "Drupal"],
+  }
+  
+];
